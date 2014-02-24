b0678f668393d0c2eef908e637ae98f841e0ba03
smakhtin <algariol@gmail.com>
2014-02-25 03:32:34 +0400

    Initial commi

---
+++ FromJsonComponent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Grasshopper.Kernel;
+using Newtonsoft.Json.Linq;
+
+namespace ru.Mathrioshka.ghJSON
+{
+    public class FromJsonComponent : GH_Component
+    {
+
+        public FromJsonComponent():base("FromJSON", "FromJSON", "Parse JSON string", "Data", "JSON"){}
+
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("475E86C2-41FA-4F8E-9C7C-07E1E0AE06A9"); }
+        }
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("JSON", "J", "JSON string", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("JObject", "JO", "JSON Object", GH_ParamAccess.item);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess da)
+        {
+            string jsonString = null;
+
+            if (!da.GetData(0, ref jsonString)) { return; }
+
+            if(String.IsNullOrEmpty(jsonString)) return;
+
+            var jsonObject = JObject.Parse(jsonString);
+
+            da.SetData(0, jsonObject);
+        }
+    }
+}
---
+++ JPathComponent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Grasshopper.Kernel;
+using JsonPath;
+using Newtonsoft.Json.Linq;
+
+namespace ru.Mathrioshka.ghJSON
+{
+    public class JPathComponent : GH_Component
+    {
+        private JsonPathContext FParser = new JsonPathContext { ValueSystem = new JsonNetValueSystem() };
+       
+        public JPathComponent():base("JPath", "JPath", "Get data from JObject by JPath query", "Extra", "JSON") {}
+
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("82E4C254-1A3E-457F-AD8B-80A824E3A53E"); }
+        }
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("JObject", "JO", "JSON Object", GH_ParamAccess.item);
+            pManager.AddTextParameter("JPath Query", "JPQ", "JPath query for the needed data", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Data", "D", "Result Data", GH_ParamAccess.list);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess da)
+        {
+            JObject jsonObject = null;
+            string jpathQuery = null;
+
+            if (!da.GetData(0, ref jsonObject) || !da.GetData(1, ref jpathQuery)) return;
+
+            if (jsonObject == null || String.IsNullOrEmpty(jpathQuery)) return;
+
+            var values = FParser.SelectNodes(jsonObject, jpathQuery).Select(node => node.Value.ToString()).ToList();
+
+            da.SetDataList(0, values);
+        }
+    }
+}
